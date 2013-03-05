8dca25f4b5609c3d3a8ab2b04459ab687666d627
Alexander Pavlov <alx-xc@yandex.ru>
2013-03-05 19:49:49 +0400

    stats formating moved to erpher_rt_stat

--- src/ejobman_print_stat.erl
+++ src/ejobman_print_stat.erl
@@ -57,13 +57,7 @@
 -spec make_stat_t_info(#ejm{}, text | html) -> string() | list().
 
 make_stat_t_info(St, raw) ->
-    get_stat_t_info(St);
-
-make_stat_t_info(St, text) ->
-    make_stat_t_info_text(St);
-
-make_stat_t_info(St, html) ->
-    make_stat_t_info_html(St).
+    get_stat_t_info(St).
 
 %%-----------------------------------------------------------------------------
 %%
@@ -88,76 +82,6 @@ make_stat_cur_info(St) ->
 %%%----------------------------------------------------------------------------
 %%% Internal functions
 %%%----------------------------------------------------------------------------
-%%
-%% @doc creates time statistic as html
-%%
-make_stat_t_info_html(St) ->
-    List = get_stat_t_info(St),
-    F = fun({{Dt, Group}, {W_cur, W_max, Q_cur, Q_max}}) ->
-                [
-                 "<tr>",
-                 "<td>", mpln_misc_time:make_str2_int(Dt), "</td>",
-                 "<td>", mpln_misc_web:make_string(Group), "</td>",
-                 "<td>", mpln_misc_web:make_string(W_cur), "</td>",
-                 "<td>", mpln_misc_web:make_string(W_max), "</td>",
-                 "<td>", mpln_misc_web:make_string(Q_cur), "</td>",
-                 "<td>", mpln_misc_web:make_string(Q_max), "</td>",
-                 "</tr>\n"
-                ];
-           ({K, V}) ->
-                [
-                 "<tr>",
-                 "<td colspan=2>",
-                 mpln_misc_web:make_term_string(K),
-                 "</td>",
-                 "<td colspan=4>",
-                 mpln_misc_web:make_term_string(V),
-                 "</td>",
-                 "</tr>\n"
-                ]
-        end,
-    F_big = fun({Tag, L}) ->
-                    [
-                     "<html><body>\n<p>\n",
-                     "<p>\n",
-                     "<table ", ?TABC, ">",
-                     "<tr><td colspan=6 bgcolor=\"#CCCCDA\">",
-                     mpln_misc_web:make_term_string(Tag),
-                     "</td></tr>\n",
-                     "<tr>",
-                     "<td>time</td>",
-                     "<td>group</td>",
-                     "<td>work current</td>",
-                     "<td>work max</td>\n",
-                     "<td>queue current</td>",
-                     "<td>queue max</td>",
-                     "</tr>\n",
-                     lists:map(F, L),
-                     "<table>\n",
-                     "<p>\n",
-                     "</body></html>\n"
-                    ]
-            end,
-    lists:flatten(lists:map(F_big, List)).
-
-%%-----------------------------------------------------------------------------
-%%
-%% @doc creates time statistic as text
-%%
-make_stat_t_info_text(St) ->
-    List = get_stat_t_info(St),
-    F = fun({K, V}) ->
-                io_lib:format("~p: ~p~n", [K, V])
-        end,
-    F_big = fun({Tag, L}) ->
-                    [io_lib:format("~p~n~n", [Tag]),
-                     lists:map(F, L),
-                     io_lib:format("----------------------------------------"
-                                   "~n~n", [])
-                    ]
-            end,
-    lists:flatten(lists:map(F_big, List)).
-
 %%-----------------------------------------------------------------------------
 %%
 %% @doc fetches queue names and sizes
